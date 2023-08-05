c1d17b989e17a81a5a02e91ca13e2635333d78ae
Jaycee Lock <jaycee.lock@gmail.com>
2023-08-05 13:05:41 +0100

    Added basic account struct and compound interest calculator, with test accounts. Need to devise way to effeciently allocate resources between accounts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+enum AccountType {
+    ASSET,
+    LIABILITY,
+    CASH,
+}
+
+struct Account<U: Into<f64>, T: Into<f64>> {
+    amount: U,
+    growth_rate: T,
+    account_type: AccountType,
+}
+
+impl<U: Into<f64> + Copy, T: Into<f64> + Copy> Account<U, T> {
+    fn calculate_annual_growth(&self) -> f64 {
+        // Use compound interest
+        let total = match self.account_type {
+            AccountType::ASSET => {
+                self.amount.into() * (1. + (self.growth_rate.into() / 100. / 12.)).powf(12.)
+            }
+            AccountType::CASH => {
+                self.amount.into() * (1. + (-self.growth_rate.into() / 100. / 12.)).powf(12.)
+            }
+            AccountType::LIABILITY => {
+                self.amount.into() * (1. + (-self.growth_rate.into() / 100. / 12.)).powf(12.)
+            }
+        };
+
+        return total - self.amount.into();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cash_account = Account {
+        amount: 10_000,
+        growth_rate: 7.0,
+        account_type: AccountType::CASH,
+    };
+
+    let first_direct_account = Account {
+        amount: 0.00,
+        growth_rate: 0.01,
+        account_type: AccountType::ASSET,
+    };
+
+    let mortgage_account = Account {
+        amount: 50_000,
+        growth_rate: 5.0,
+        account_type: AccountType::LIABILITY,
+    };
+
+    let cash_growth = cash_account.calculate_annual_growth();
+    let mortgage_growth = mortgage_account.calculate_annual_growth();
+    let savings_growth = first_direct_account.calculate_annual_growth();
+
+    println!("Cash growth: £{:.2}", cash_growth);
+    println!("Mortgage growth: £{:.2}", mortgage_growth);
+    println!("Saving growth growth: £{:.2}", savings_growth);
 }
